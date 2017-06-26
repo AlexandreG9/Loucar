94bb3eefdcbcb37c982b367db94e9198a2bc8d72
aguiheneuf2015 <aguiheneuf@gmail.com>
2017-06-26 11:44:41 +0200

    Ajout du model

---
+++ app/src/main/java/aguiheneuf/gsiaudeau/loucar/model/Location.java
@@ -0,0 +1,17 @@
+package aguiheneuf.gsiaudeau.loucar.model;
+
+import java.util.Date;
+
+public class Location {
+	public long id;
+
+	public Voiture voiture;
+
+	public Client client;
+
+	public Date dateDebut;
+
+	public Date dateFin;
+
+
+}
---
+++ app/src/main/java/aguiheneuf/gsiaudeau/loucar/model/Marque.java
@@ -0,0 +1,9 @@
+package aguiheneuf.gsiaudeau.loucar.model;
+
+public class Marque {
+
+	public long id;
+
+	public String nom;
+
+}
