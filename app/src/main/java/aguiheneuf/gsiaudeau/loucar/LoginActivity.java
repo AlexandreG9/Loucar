718f0100152583317933640ab0fb5cb3d5082945
WaterBucket <slothdragn@gmail.com>
2017-06-26 15:52:44 +0200

    Test

--- app/src/main/java/aguiheneuf/gsiaudeau/loucar/LoginActivity.java
+++ app/src/main/java/aguiheneuf/gsiaudeau/loucar/LoginActivity.java
@@ -1,13 +1,29 @@
 package aguiheneuf.gsiaudeau.loucar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static String Ressource;
+    private static Integer REQUEST_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+        public void OnClickConnect(View view) {
+        Intent intent = new Intent(LoginActivity.this, ListeActivity.class);
+        intent.putExtra("id", "abcd");
+        Ressource = "Connecté";
+        // Inflate the menu; this adds items to the action bar if it is present.
+        Toast.makeText(LoginActivity.this, Ressource, Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+    }
+
 }
